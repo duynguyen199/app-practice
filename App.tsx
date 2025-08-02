438dbb19d244f2eef6903c704c5105b366443cf9
duynguyen <dannynguyen748@gmail.com>
2025-08-02 15:48:19 -0400

    react native

--- App.tsx
+++ App.tsx
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Alert,
+  Button,
+  Image,
+  Platform,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const onButtonPress = () => Alert.alert("Pressed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        horizontal={false}
+        contentContainerStyle={{
+            backgroundColor: "black"
+        }}
+      >
+        <Text style={styles.textHeader}>Hello</Text>
+        <Text numberOfLines={3}>
+          Lorem ipsum dolor sit a
+          <Text
+            style={styles.textSubHeader}
+            onPress={() => {
+              Alert.alert("Press");
+            }}
+          >
+            Press Here
+          </Text>
+          met consectetur adipisicing elit. Dolor, fugiat iusto. Neque odio
+          omnis ea porro sed repellat et deserunt iure ducimus nulla, facilis
+          maxime illum fugiat numquam magni blanditiis.
+        </Text>
+        <StatusBar style="auto" />
+
+        <Pressable onPress={onButtonPress}>
+          <Image
+            style={styles.image1}
+            source={require("./assets/adaptive-icon.png")}
+          />
+        </Pressable>
+
+        <TouchableOpacity>
+          <Image style={styles.image1} source={require("./assets/tiger.jpg")} />
+        </TouchableOpacity>
+         <TouchableOpacity>
+          <Image style={styles.image1} source={require("./assets/tiger.jpg")} />
+        </TouchableOpacity>
+         <TouchableOpacity>
+          <Image style={styles.image1} source={require("./assets/tiger.jpg")} />
+        </TouchableOpacity>
+         <TouchableOpacity>
+          <Image style={styles.image1} source={require("./assets/tiger.jpg")} />
+        </TouchableOpacity>
+         <TouchableOpacity>
+          <Image style={styles.image1} source={require("./assets/tiger.jpg")} />
+        </TouchableOpacity>
+         <TouchableOpacity>
+          <Image style={styles.image1} source={require("./assets/tiger.jpg")} />
+        </TouchableOpacity>
+
+        <Button title="Press On Me" onPress={onButtonPress} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Platform.OS === "android"? "black": "aqua",
+  },
+  textHeader: {
+    textAlign: "center",
+    fontSize: 55,
+    fontWeight: "bold",
+  },
+  textSubHeader: {
+    color: Platform.OS==="android"? "yellow": "red",
+  },
+  image1: {
+    height: 200,
+    width: 200,
   },
 });
